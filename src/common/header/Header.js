fb04b7ffb3fef7815394d6d0e74e2a2d24ebb916
Ameer Khan <Ameer.Khan@xome.com>
2021-11-27 19:42:02 +0530

    Login and Register Forms Completed

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,39 +1,74 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
-import './Header.css';
+import SignInSignUpModal from "../modal/Modal";
+import "./Header.css";
 
 function renderLoginLogoutButton() {
-    if(sessionStorage.getItem("access-token")) {      
-        return <Button className="header-button" variant="contained">Logout</Button>;
-    }
-    else {
-        return <Button className="header-button" variant="contained">Login</Button>;
-    }
-
+  if (sessionStorage.getItem("access-token")) {
+    return (
+      <Button className="header-button" variant="contained">
+        Logout
+      </Button>
+    );
+  } else {
+    return (
+      <Button className="header-button" variant="contained">
+        Login
+      </Button>
+    );
+  }
 }
 
-function renderBookShowButton(id) {
-    if(sessionStorage.getItem("access-token")) {
-        return (
-            <Link to={"/bookshow/" + id}>
-                <Button className="header-button" color="primary" variant="contained" style={{marginRight: "5px"}}>Book Show</Button>
-            </Link>
-        );
-    }
-    else {
-        // TODO - When user is not logged in, render button that opens signin/signup modal
-    }
+function renderBookShowButton(id, setShowModal) {
+  if (sessionStorage.getItem("access-token")) {
+    return (
+      <Link to={"/bookshow/" + id}>
+        <Button
+          className="header-button"
+          color="primary"
+          variant="contained"
+          style={{ marginRight: "5px" }}
+        >
+          Book Show
+        </Button>
+      </Link>
+    );
+  } else {
+    return (
+      <Button
+        onClick={() => setShowModal(true)}
+        className="header-button"
+        color="primary"
+        variant="contained"
+        style={{ marginRight: "5px" }}
+      >
+        Book Show
+      </Button>
+    );
+  }
 }
 
 const Header = (props) => {
-    return (
-    <div className="header">
-        <img className="logo" src={require('../../assets/logo.svg')} alt="logo" />
+  const [showModal, setShowModal] = useState(false);
+  const closeModalHandler = () => setShowModal(false);
+
+  return (
+    <React.Fragment>
+      <div className="header">
+        <img
+          className="logo"
+          src={require("../../assets/logo.svg")}
+          alt="logo"
+        />
         {renderLoginLogoutButton()}
-        {props.isDetailsPage ? renderBookShowButton(props.match.params.id) : null}
-    </div>
-    );
-}
+        {props.isDetailsPage
+          ? renderBookShowButton(props.match.params.id, setShowModal)
+          : null}
+      </div>
+      <SignInSignUpModal show={showModal} close={closeModalHandler} />
+    </React.Fragment>
+  );
+};
 
 export default Header;
---
+++ src/common/modal/Modal.js
@@ -0,0 +1,312 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { useTheme } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import FormHelperText from "@mui/material/FormHelperText";
+import Button from "@mui/material/Button";
+
+export default function SignInSignUpModal({ show, close }) {
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+  return (
+    <div className="modal">
+      <Modal
+        isOpen={show}
+        contentLabel="Example Modal"
+        style={customStyles}
+        ariaHideApp={false}
+      >
+        <FullWidthTabs />
+      </Modal>
+    </div>
+  );
+}
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 2 }}>
+          <Typography component="div">{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
+
+function FullWidthTabs() {
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ bgcolor: "background.paper", width: 350 }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="secondary"
+        textColor="inherit"
+        variant="fullWidth"
+        aria-label="login register tabs"
+        centered
+      >
+        <Tab label="LOGIN" {...a11yProps(0)} />
+        <Tab label="REGISTER" {...a11yProps(1)} />
+      </Tabs>
+      <TabPanel value={value} index={0} dir={theme.direction}>
+        <LoginForm />
+      </TabPanel>
+      <TabPanel value={value} index={1} dir={theme.direction}>
+        <RegisterForm />
+      </TabPanel>
+    </Box>
+  );
+}
+
+function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [usernameValid, isUsernameValid] = useState(true);
+  const [passwordValid, isPasswordValid] = useState(true);
+  const [validLoginDetails, isValidLoginDetails] = useState(false);
+
+  function onSubmitHandler(e) {
+    if (username) isUsernameValid(true);
+    else isUsernameValid(false);
+    if (password) isPasswordValid(true);
+    else isPasswordValid(false);
+  }
+
+  const formControlStyle = {
+    margin: "10px"
+  };
+
+  const labelStyle = {
+    left: "-14px"
+  };
+
+  const buttonStyle = {
+    marginTop: "40px"
+  };
+
+  const helperTextStyle = {
+    marginLeft: "0px"
+  }
+
+  return (
+    <Grid align="center">
+      <FormControl style={formControlStyle} required>
+        <InputLabel style={labelStyle} color="secondary" htmlFor="username">
+          Username
+        </InputLabel>
+        <Input
+          id="username"
+          value={username}
+          color="secondary"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        {!usernameValid && <FormHelperText style={helperTextStyle} error>required</FormHelperText>}
+      </FormControl>
+      <FormControl style={formControlStyle} required>
+        <InputLabel style={labelStyle} color="secondary" htmlFor="password">
+          Password
+        </InputLabel>
+        <Input
+          id="password"
+          value={password}
+          color="secondary"
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        {!passwordValid && <FormHelperText style={helperTextStyle} error>required</FormHelperText>}
+      </FormControl>
+      <Typography style={buttonStyle}>
+        <Button
+          variant="contained"
+          onClick={onSubmitHandler}
+        >
+          LOGIN
+        </Button>
+      </Typography>
+    </Grid>
+  );
+}
+
+function RegisterForm() {
+  const [firstNameValid, isFirstNameValid] = useState(true);
+  const [lastnameValid, isLastNameValid] = useState(true);
+  const [emailValid, isEmailValid] = useState(true);
+  const [passwordValid, isPasswordValid] = useState(true);
+  const [contactValid, isContactValid] = useState(true);
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [contact, setContact] = useState("");
+
+  const [validRegistrationDetails, isValidRegistrationDetails] = useState(false);
+
+  function onSubmitHandler(e) {
+    if (firstName) isFirstNameValid(true);
+    else isFirstNameValid(false);
+    if (lastName) isLastNameValid(true);
+    else isLastNameValid(false);
+    if (email) isEmailValid(true);
+    else isEmailValid(false);
+    if (password) isPasswordValid(true);
+    else isPasswordValid(false);
+    if (contact) isContactValid(true);
+    else isContactValid(false);
+    if(firstName && lastName && email && password && contact) isValidRegistrationDetails(true);
+    else isValidRegistrationDetails(false);
+  }
+
+  const formControlStyle = {
+    margin: "10px"
+  };
+
+  const labelStyle = {
+    left: "-14px"
+  };
+
+  const buttonStyle = {
+    marginTop: "40px"
+  };
+
+  const helperTextStyle = {
+    marginLeft: "0px"
+  }
+
+  return (
+    <Grid align="center">
+      <FormControl style={formControlStyle} required>
+        <InputLabel style={labelStyle} color="secondary" htmlFor="firstName">
+          First Name
+        </InputLabel>
+        <Input
+          id="firstName"
+          value={firstName}
+          color="secondary"
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
+        />
+        {!firstNameValid && <FormHelperText style={helperTextStyle} error>required</FormHelperText>}
+      </FormControl>
+      <FormControl style={formControlStyle} required>
+        <InputLabel style={labelStyle} color="secondary" htmlFor="lastName">
+          Last Name
+        </InputLabel>
+        <Input
+          id="lastName"
+          value={lastName}
+          color="secondary"
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+        />
+        {!lastnameValid && <FormHelperText style={helperTextStyle} error>required</FormHelperText>}
+      </FormControl>
+      <FormControl style={formControlStyle} required>
+        <InputLabel style={labelStyle} color="secondary" htmlFor="email">
+          Email
+        </InputLabel>
+        <Input
+          id="email"
+          value={email}
+          color="secondary"
+          type="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        {!emailValid && <FormHelperText style={helperTextStyle} error>required</FormHelperText>}
+      </FormControl>
+      <FormControl style={formControlStyle} required>
+        <InputLabel style={labelStyle} color="secondary" htmlFor="password">
+          Password
+        </InputLabel>
+        <Input
+          id="password"
+          value={password}
+          color="secondary"
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        {!passwordValid && <FormHelperText style={helperTextStyle} error>required</FormHelperText>}
+      </FormControl>
+      <FormControl style={formControlStyle} required>
+        <InputLabel style={labelStyle} color="secondary" htmlFor="contact">
+          Contact No.
+        </InputLabel>
+        <Input
+          id="contact"
+          value={contact}
+          color="secondary"
+          type="number"
+          inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+          onChange={(e) => {
+            setContact(e.target.value);
+          }}
+        />
+        {!contactValid && <FormHelperText style={helperTextStyle} error>required</FormHelperText>}
+      </FormControl>
+      {validRegistrationDetails && <p>Registration Successful. Please Login!</p>}
+      <Typography style={buttonStyle}>
+        <Button
+          variant="contained"
+          onClick={onSubmitHandler}
+        >
+          REGISTER
+        </Button>
+      </Typography>
+    </Grid>
+  );
+}
