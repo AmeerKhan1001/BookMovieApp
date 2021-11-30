5dead8520879c8bdff1d9005d6bb0766f3662181
Ameer Khan <Ameer.Khan@xome.com>
2021-11-30 18:53:46 +0530

    Released Movies

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Header from "../../common/header/Header";
 import "./Home.css";
-import { createStyles, makeStyles } from '@mui/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
+import { createStyles, makeStyles } from "@mui/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
 
 const Home = (props) => {
   return (
     <React.Fragment>
       <Header />
       <HomeBanner />
-      <UpcomingMovies baseUrl={props.baseUrl}/>
+      <UpcomingMovies baseUrl={props.baseUrl} />
+      <ReleasedMoviesSection {...props} baseUrl={props.baseUrl} />
     </React.Fragment>
   );
 };
@@ -25,56 +26,57 @@ function HomeBanner() {
 }
 
 function UpcomingMovies(props) {
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
 
-    const [upcomingMovies, setUpcomingMovies] = useState([]);
+  const useStyles = makeStyles((theme) =>
+    createStyles({
+      root: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+        overflow: "hidden",
+        backgroundColor: "white",
+      },
+      gridList: {
+        flexWrap: "nowrap",
+        transform: "translateZ(0)",
+        width: "100%",
+      },
+      title: {
+        color: "#fff",
+      },
+      titleBar: {
+        background: "rgba(0, 0, 0, 0.5)",
+      },
+    })
+  );
 
-    const useStyles = makeStyles(theme =>
-        createStyles({
-          root: {
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-around',
-            overflow: 'hidden',
-            backgroundColor: 'white',
-          },
-          gridList: {
-            flexWrap: 'nowrap',
-            transform: 'translateZ(0)',
-            width: '100%'
-          },
-          title: {
-            color: '#fff'
-          },
-          titleBar: {
-            background: 'rgba(0, 0, 0, 0.5)'
-          },
-        }),
-    );
+  const classes = useStyles();
 
-    const classes = useStyles();
+  useEffect(() => {
+    /* Fetching Upcoming Movies */
 
-    useEffect(() => {
-      
-      /* Fetching Upcoming Movies */
+    fetch(props.baseUrl + "movies?status=PUBLISHED", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => setUpcomingMovies(response.movies));
+  }, []);
 
-      fetch(props.baseUrl + "movies?status=PUBLISHED", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "Cache-Control": "no-cache",
-        },
-      })
-      .then(response => response.json())
-      .then(response => setUpcomingMovies(response.movies));
-
-    }, []);
-
-    return(
-      <div className={classes.root}>
+  return (
+    <div className={classes.root}>
       <GridList cellheight={250} cols={6} className={classes.gridList}>
-        {upcomingMovies.map(movie => (
-          <GridListTile key={"movie_"+movie.id}>
-            <img src={movie.poster_url} alt={movie.title} className="movie-image"/>
+        {upcomingMovies.map((movie) => (
+          <GridListTile key={"movie_" + movie.id}>
+            <img
+              src={movie.poster_url}
+              alt={movie.title}
+              className="movie-image"
+            />
             <GridListTileBar
               title={movie.title}
               classes={{
@@ -86,7 +88,79 @@ function UpcomingMovies(props) {
         ))}
       </GridList>
     </div>
-    );
+  );
+}
+
+function ReleasedMoviesSection(props) {
+  const [releasedMovies, setReleasedMovies] = useState([]);
+  const [genresFilter, setGenresFilter] = useState([]);
+  const [artistsFilter, setArtistsFilter] = useState([]);
+
+  useEffect(() => {
+    /* Fetch released movies */
+    fetch(props.baseUrl + "movies?status=RELEASED", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => setReleasedMovies(response.movies));
+
+    /* Fetch genres */
+    fetch(props.baseUrl + "genres", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => setGenresFilter(response.genres));
+
+    /* Fetch artists */
+    fetch(props.baseUrl + "artists", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => setArtistsFilter(response.artists));
+  }, []);
+
+  return (
+    <div className="flex-container">
+      <div className="released-movies">
+        <GridList cellHeight={350} cols={4} spacing={20}>
+          {releasedMovies.map((movie) => (
+            <GridListTile
+              key={"movie_" + movie.id}
+              style={{ height: "350px", width: "300px", cursor:"pointer" }}
+              onClick={() => props.history.push("/movie/" + movie.id)}
+            >
+              <img
+                src={movie.poster_url}
+                alt={movie.title}
+                className="movie-image"
+              />
+              <GridListTileBar
+                title={movie.title}
+                subtitle={
+                  <span>
+                    Release Date: {new Date(movie.release_date).toDateString()}
+                  </span>
+                }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+      <div className="movie-filters"></div>
+    </div>
+  );
 }
 
 export default Home;
